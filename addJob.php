f4b987282b6cfc4a727c1336f2397985729ac413
David <dsanva2004@gmail.com>
2020-07-21 11:43:55 -0500

    fix: form modifications

--- addJob.php
+++ addJob.php
@@ -1,14 +1,19 @@
 <?php
 //Variables superglobales
-var_dump($_POST);
+//var_dump($_POST);
+//var_dump($_GET);
 ?>
 
 <html>
     <head>
         <title>Add Job</title>
-    </head>
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B"
+              crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+     </head>
     <body>
-
+    <h1>Add Job</h1>
     <form action="addJob.php" method="post">
         <label for="">Title:</label>
         <input type="text" name="title"> <br>
